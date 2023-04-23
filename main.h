f60724f823ecebc4863d2dc889cac5f5aca37ccf
Berivero <berylouma48@gmail.com>
2023-04-23 08:02:50 +0000

    first commit

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*function prototypes */
+int _printf(const char *format, ...);
+
+
+
+
+/*standard library */
+#include <stdarg.h>
+#include <unistd.h>
+
+
+#endif
