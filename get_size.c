3909b0899ed671c2b21795c87de6640e2e1af1de
Berivero <berylouma48@gmail.com>
2023-04-25 11:08:36 +0000

    project printf

---
+++ get_size.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * get_size - prints the size of precision argument
+ * @format: format to print the arguments
+ * @i: number of arguments to be printed
+ *
+ * Return: precision size
+ */
+int get_size(const char *format, int *i)
+{
+	int curr_i = *i + 1;
+	int size = 0;
+
+	if (format[curr_i] == 'l')
+		size = S_LONG;
+	else if (format[curr_i] == 'h')
+		size = S_SHORT;
+
+	if (size == 0)
+		*i = curr_i - 1;
+	else
+		*i = curr_i;
+
+	return (size);
+}
